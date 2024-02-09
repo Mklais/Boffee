812977a1a6ad1f95405265d8363cf7894bd5ed81
Mklais <129872554+Mklais@users.noreply.github.com>
2024-02-09 17:13:01 +0200

    09/02/24 DB-SQL-Scripts rework
    Deletion of unnecessary sql scripts

--- bookclub/sql-scripts/12_01 Create friendship table.sql
+++ bookclub/sql-scripts/12_01 Create friendship table.sql
@@ -1,21 +1,14 @@
 USE bookclub_database;
 
--- Create the friendship table with indexes
+-- Create Friendship table
 CREATE TABLE IF NOT EXISTS friendship (
     friendship_id INT AUTO_INCREMENT PRIMARY KEY,
-    user1_id INT NOT NULL,
-    user2_id INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_friendship_user1 FOREIGN KEY (user1_id) 
-        REFERENCES users (user_id) 
-        ON DELETE CASCADE,
-    CONSTRAINT fk_friendship_user2 FOREIGN KEY (user2_id) 
-        REFERENCES users (user_id) 
-        ON DELETE CASCADE,
-    CHECK (user1_id <> user2_id) -- Ensure users are not friends with themselves
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    sent_at TIMESTAMP NOT NULL,
+    responded_at TIMESTAMP NULL,
+    CONSTRAINT fk_sender_id FOREIGN KEY (sender_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_receiver_id FOREIGN KEY (receiver_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CHECK (sender_id != receiver_id) -- Ensure a user cannot send a friend request to themselves
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Add indexes to improve search performance
-CREATE INDEX idx_friendship_user1 ON friendship (user1_id);
-CREATE INDEX idx_friendship_user2 ON friendship (user2_id);
-CREATE INDEX idx_friendship_status ON friendship (status);
